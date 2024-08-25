e13deed6844f2a2980759644ccf2422d6e121f88
alexey-artsiush <artush.alexey@gmail.com>
2024-08-25 17:55:33 +0300

    signup, signin, signout

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { CookieService } from './cookie.service';
+import { JwtService } from '@nestjs/jwt';
+import * as process from 'node:process';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest() as Request;
+    const token = req.cookies[CookieService.tokenKey];
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const sessionInfo = this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SESSION_SECRET,
+      });
+
+      req['session'] = sessionInfo;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
